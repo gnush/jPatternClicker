8af116942297e65b7d1c82f9ea7d60599316ce7b
gnush <1129861+gnush@users.noreply.github.com>
2025-05-19 12:20:46 +0200

    Launcher

---
+++ README.md
@@ -0,0 +1,14 @@
+# jPatternClicker
+
+Record Mouse Click Patterns and replay them to give your carpal tunnel some rest from bad UI.
+
+## Requires
+
+* Java >= 21
+
+## Error: JavaFX runtime components are missing, and are required to run this application
+
+Install a JDK that bundles JavaFX or install OpenJFX
+
+* https://wiki.openjdk.java.net/display/OpenJFX/Main
+* https://openjfx.io/
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
-    id("application")
+    application
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "io.github.gnush"
 version = "1.0.0"
+
+val mainClassName = "io.github.gnush.patternclicker.MainApp"
+val launcherClass = "io.github.gnush.patternclicker.Launcher"
 
 repositories {
     mavenCentral()
@@ -15,10 +18,25 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    runtimeOnly("org.openjfx:javafx-graphics:${javafx.version}:linux")
+    runtimeOnly("org.openjfx:javafx-graphics:${javafx.version}:win")
+    runtimeOnly("org.openjfx:javafx-graphics:${javafx.version}:mac")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes("Main-Class" to launcherClass)
+    }
+    from(
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    )
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 javafx {
@@ -27,7 +45,7 @@ javafx {
 }
 
 application {
-    mainClass = "io.github.gnush.patternclicker.MainApp"
+    mainClass = mainClassName
 //    applicationDefaultJvmArgs = listOf(
 //        "--enable-native-access=javafx.graphics,ALL-UNNAMED",
 //        "--sun-misc-unsafe-memory-access=allow" // remove when https://bugs.openjdk.org/browse/JDK-8345121 has been fixed
